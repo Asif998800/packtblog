5ee3db451de32f6935d82e4e62adfd25653c36c9
Asif998800 <asif018600@gmail.com>
2019-11-16 16:28:06 +0530

    Add pagination

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,22 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'list.html', {'posts': posts})
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+
+    # Handle out of range and invalid page numbers:
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,'list.html',{'posts': posts})
 
 def post_detail(request, year, month, day, slug):
     post = Post.objects.get(slug=slug,

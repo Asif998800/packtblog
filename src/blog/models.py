eeff5d0b832152126b9f50d54534c675fdc200c0
Asif998800 <asif018600@gmail.com>
2019-11-16 13:14:15 +0530

    Add post_list,post_detail,get_absolute_url

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Post(models.Model):
@@ -20,3 +21,9 @@ class Post(models.Model):
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.publish.year,
+                                            self.publish.month,
+                                            self.publish.day,
+                                            self.slug])
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, slug):
+    post = Post.objects.get(slug=slug,
+                            status='published',
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+    return render(request, 'detail.html', {'post': post})

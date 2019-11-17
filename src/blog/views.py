75692000e0b3e89dbe11c46d980ea135b380759f
Asif998800 <asif018600@gmail.com>
2019-11-17 18:25:47 +0530

    Added Comment functionality

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,8 +2,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 
@@ -34,7 +34,27 @@ def post_detail(request, year, month, day, slug):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
-    return render(request, 'detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'detail.html', {'post': post, 
+                                            'comments': comments,
+                                            'new_comment': new_comment,
+                                            'comment_form': comment_form})
 
 def post_share(request, id):
     post = get_object_or_404(Post, id=id)
@@ -57,6 +77,3 @@ def post_share(request, id):
     return render(request, 'share.html', {'post': post, 'form': form, 'sent': sent})
 
 
-
-# message = 'Read "{}" at {}\n\n{}\'s comments:\{}'.format(post.title, post_url, cd['name'], cd['comments'])
-# subject = '{} ({}) recommends you reading "\{}"'.format(cd['name'], cd['email'], post.title)
